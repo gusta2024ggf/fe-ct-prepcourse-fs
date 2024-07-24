3546a741264ddfc68b9474712ed712b576c67182
gustavo fabris <gfcero4@gmail.com>
2024-07-24 14:38:41 -0300

    Update 07.js

--- C5 Fundamentos Javascript/ejercicios/07.js
+++ C5 Fundamentos Javascript/ejercicios/07.js
@@ -1,8 +1,11 @@
-function esTipoDato(valor) {
+function esTipoDato(number) {
   // La función recibe un argumento llamado valor.
   // Retorna el tipo de dato de este valor.
   // Por ejemplo: "string", "number", "boolean", "object", etc.
   // Tu código:
+  var a = 8 + 6;
+  var b = 4 - 2;
+  var c = a + b;
 }
 
 module.exports = esTipoDato;
