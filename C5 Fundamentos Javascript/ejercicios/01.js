b1cb94e5b4b9037a14601364baeb81b911dbd94b
gustavo fabris <gfcero4@gmail.com>
2024-07-24 21:33:40 -0300

    Update 01.js

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,6 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString= 'hola, mundo!;
+const nuevoString= 'null';
 
-module.exports = toString;
+module.exports = nuevoString;
