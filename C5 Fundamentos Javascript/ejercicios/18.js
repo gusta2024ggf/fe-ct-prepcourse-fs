570b43379ca7df6d8688a5cb949237ee0a67593a
gustavo fabris <gfcero4@gmail.com>
2024-07-25 21:09:42 -0300

    Update 18.js

--- C5 Fundamentos Javascript/ejercicios/18.js
+++ C5 Fundamentos Javascript/ejercicios/18.js
@@ -8,6 +8,14 @@ function esVocal(letra) {
   // "n" ---> "Dato incorrecto"
   // "texto largo" ---> "Dato incorrecto"
   // Tu código:
+  if (letra === 'a' || letra === 'e'||letra === 'i' || letra === 'o' || letra === 'u'){
+    return "es vocal";
+    
+}else
+  {
+    return "Dato incorrecto";
+  }
+if(letra !== n){
+  return "Dato incorrecto";
 }
-
 module.exports = esVocal;
