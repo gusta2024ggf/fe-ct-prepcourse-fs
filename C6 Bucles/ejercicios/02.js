fd81d363e3a6cc0e7e37b18c5784b19f746ca50a
gustavo fabris <gfcero4@gmail.com>
2024-07-27 05:18:41 -0300

    Update 02.js

--- C6 Bucles/ejercicios/02.js
+++ C6 Bucles/ejercicios/02.js
@@ -2,7 +2,7 @@ function esVerdaderoYFalso(booleano1, booleano2) {
   // La función recibe dos argumentos "booleano1" y "booleano2".
   // Retorna true si ambos son verdaderos, caso contrario, retorna false.
   // Tu código:
-  if(booleano1 && booleano2 === true)return true
+  if(booleano1 && booleano2)return true
 else return false
 }
 
