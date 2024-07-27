f892455e760b2282fa20adba15ffbed30de2f697
gustavo fabris <gfcero4@gmail.com>
2024-07-27 05:27:39 -0300

    Update 05.js

--- C6 Bucles/ejercicios/05.js
+++ C6 Bucles/ejercicios/05.js
@@ -2,6 +2,8 @@ function esVerdadero(valor) {
   // Si "valor" es verdadero, retorna "Soy verdadero".
   // Caso contrario, retorna "Soy falso".
   // Tu código:
+  if(valor === true)return true
+else false
 }
 
 module.exports = esVerdadero;
