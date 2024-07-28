85c132bdce74254b05a219e153b9dd42756bd14a
gusta2024ggf <gfcero4@gmail.com>
2024-07-28 07:05:04 -0300

    no me pasa

--- C7 Arrays/ejercicios/02.js
+++ C7 Arrays/ejercicios/02.js
@@ -1,6 +1,24 @@
+import combine from 'combine';
 function devolverUltimoElemento(array) {
   // Retornar el último elemento del arreglo recibido.
   // Tu código:
+
+
+
+ var finDelBucle = Math.max(str1.length,str2.length,str3.length)
+
+for(var i = 0; i < finDelBucle; i++){
+
+  var acum = ""
+  console.log(str1[i] !== undefined) acum + acum + str1[i]
+  console.log(str1[i] !== undefined) acum + acum + str2[i]
+  console.log(str3[i] !== undefined) acum + acum + str3[i]
+
+if(str1[i] !== undefined) acum = acum + str1[i]
+if(str2[i] !== undefined) acum =  acum + str2[i]
+if(str3[i] !== undefined) acum =  acum + str3[i]
+}
+return acum
 }
 
 module.exports = devolverUltimoElemento;
