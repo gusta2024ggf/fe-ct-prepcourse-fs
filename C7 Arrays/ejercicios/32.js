9ddeab1140fcb3496410981ff98fb5f3f5e421d1
gusta2024ggf <gfcero4@gmail.com>
2024-07-28 13:31:19 -0300

    modificado

--- C7 Arrays/ejercicios/32.js
+++ C7 Arrays/ejercicios/32.js
@@ -5,6 +5,12 @@ function esPalindromo(string) {
   // IMPORTANTE: Un palíndromo es una palabra o frase
   // que se lee igual hacia adelante que hacia atrás.
   // Tu código:
+  var stringInvertido = string.split("").reverse().join("")
+
+
+ return string.toLowercase() === stringInvertido.toLowercase()
 }
+
+
 
 module.exports = esPalindromo;
