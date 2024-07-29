d9a191599a31d7743fcc83b65d6c7bfdf9585e6b
gustavo fabris <gfcero4@gmail.com>
2024-07-29 09:19:20 -0300

    Update 01.js

--- C8 Objects + Callbacks/callbacks/ejercicios/01.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/01.js
@@ -2,6 +2,10 @@ function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // NOTA: no debes retornar nada.
    // Tu código:
+function devuelvoSaludo() {
+    return 'Hola';
+ }
+ function saludar(cb1, cb2) {}
 }
 
 module.exports = invocarCallback;
