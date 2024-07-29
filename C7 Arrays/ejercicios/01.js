d9fd168966c25edceb20885cb0ee8fababee5851
gustavo fabris <gfcero4@gmail.com>
2024-07-29 04:47:10 -0300

    Update 01.js

--- C7 Arrays/ejercicios/01.js
+++ C7 Arrays/ejercicios/01.js
@@ -1,6 +1,9 @@
 function devolverPrimerElemento(array) {
   // Retornar el primer elemento del arreglo recibido.
   // Tu código:
-  return array[0];
+  
+ var numeros = [0,1,2,3];
+  var incluye = (array.includes(3));
+console.log(incluye);
 }
 module.exports = devolverPrimerElemento;
