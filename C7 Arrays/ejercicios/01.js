482c54d5c86e29193d0cf6cbfff72c7822ea00f1
gusta2024ggf <gfcero4@gmail.com>
2024-07-28 23:24:20 -0300

    modificado1

--- C7 Arrays/ejercicios/01.js
+++ C7 Arrays/ejercicios/01.js
@@ -1,15 +1,6 @@
 function devolverPrimerElemento(array) {
   // Retornar el primer elemento del arreglo recibido.
   // Tu código:
-
-  var array = [];
-  let num;
-    for (let i = 0; i < 5; i++){
-     if (num[i] === 0){
-      console.log(array[0]);
-    }
- 
-  }
+  return array[0];
 }
-
 module.exports = devolverPrimerElemento;
--- C7 Arrays/ejercicios/03.js
+++ C7 Arrays/ejercicios/03.js
@@ -1,6 +1,7 @@
 function obtenerLargoDelArray(array) {
   // Retornar la longitud del arreglo recibido.
   // Tu código:
-}
+  return array.lenght[1,2,3];
+;}
 
 module.exports = obtenerLargoDelArray;
