7b8d576bb32956167e4ce3e2c2fdc643bbdae98f
gustavo fabris <gfcero4@gmail.com>
2024-07-30 14:53:22 -0300

    Update 03.js

--- C7 Arrays/ejercicios/03.js
+++ C7 Arrays/ejercicios/03.js
@@ -1,6 +1,8 @@
 function obtenerLargoDelArray(array) {
   // Retornar la longitud del arreglo recibido.
   // Tu código:
+  var longitud;
+  longitud.lenght(array);
 }
 
 module.exports = obtenerLargoDelArray;
