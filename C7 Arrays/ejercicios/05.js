3fbd906cdc82ef4a8f8d7424d982be18128fc04f
gustavo fabris <gfcero4@gmail.com>
2024-07-30 15:37:31 -0300

    Update 05.js

--- C7 Arrays/ejercicios/05.js
+++ C7 Arrays/ejercicios/05.js
@@ -2,6 +2,14 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // Agrega el "elemento" al comienzo del arreglo recibido.
   // Retorna el arreglo.
   // Tu código:
+   let recorreElemento;
+  for recorreElemento=(i = 0,i === elemento,i++){
+array.push(0); 
+  return array;
+
+  }
+
+
 }
 
 module.exports = agregarItemAlComienzoDelArray;
