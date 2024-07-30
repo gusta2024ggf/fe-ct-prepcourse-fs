eef9e19c3f6b3bcd4be818e006e178d615b546b6
gustavo fabris <gfcero4@gmail.com>
2024-07-30 15:18:34 -0300

    Update 04.js

--- C6 Bucles/ejercicios/04.js
+++ C6 Bucles/ejercicios/04.js
@@ -3,6 +3,12 @@ function mayoriaDeEdad(edad) {
   // Si tiene 18 años o más, retorna el string: "Allowed".
   // Caso contrario: "Not allowed".
   // Tu código:
+let edad = 20;
+ if(edad <= 18){
+   return console.log("Allowed");
+   else{
+   retrun console.log("Not Allowed");
+ }
 }
 
 module.exports = mayoriaDeEdad;
